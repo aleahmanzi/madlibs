0550214683faaa5a6f798d502c3f5251a26160fc
Aleah Manzi <aleah.manzi@gmail.com>
2016-10-20 16:07:58 +0100

    deleted unnecessary files.

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 var demo = angular.module('demo', ['ngAnimate']);
   demo.controller('Ctrl', function ($scope){
-$scope.inputOptions = true;
+  $scope.inputOptions = true;
 
- $scope.genderOne = 'her';
- $scope.genderTwo = 'she'
- $scope.genderThree = 'her'
-/// - default values
+   $scope.genderOne = 'her';
+   $scope.genderTwo = 'she'
+   $scope.genderThree = 'her'
+  /// - default values
 
- $scope.setMale = function(){
-  $scope.genderOne = 'his';
-  $scope.genderTwo = 'he'
-  $scope.genderThree = 'him'
- }; /// - set male version of paragraph
+   $scope.setMale = function(){
+    $scope.genderOne = 'his';
+    $scope.genderTwo = 'he'
+    $scope.genderThree = 'him'
+   }; /// - set male version of paragraph
 
- $scope.setFemale = function(){
-  $scope.genderOne = 'her';
-  $scope.genderTwo = 'she'
-  $scope.genderThree = 'her'
- }; /// - set female version of paragraph
+   $scope.setFemale = function(){
+    $scope.genderOne = 'her';
+    $scope.genderTwo = 'she'
+    $scope.genderThree = 'her'
+   }; /// - set female version of paragraph
 
-  $scope.userSubmit = function() {
-    //console.log("test")
-    $scope.inputOptions = false;
-  };/// - submit user selections
+    $scope.userSubmit = function() {
+      //console.log("test")
+      $scope.inputOptions = false;
+    };/// - submit user selections
 
-  $scope.restart = function() {
-    $scope.inputOptions = true;
-    $scope.word = {};
-  };/// - start game over, return to default
+    $scope.restart = function() {
+      $scope.inputOptions = true;
+      $scope.word = {};
+    };/// - start game over, return to default
 
-$scope.word = {
- maleName: '',
- jobTitle: '',
- tediousTask: '',
- dirtyTask: '',
- celebrityName: '',
- uselessSkill: '',
- adjective: '',
- obnoxiousCelebrity: '',
- hugeNumber: ''
- };
+  $scope.word = {
+   maleName: '',
+   jobTitle: '',
+   tediousTask: '',
+   dirtyTask: '',
+   celebrityName: '',
+   uselessSkill: '',
+   adjective: '',
+   obnoxiousCelebrity: '',
+   hugeNumber: ''
+   };
 
-}); /// - Ctrl
+  }); /// - Ctrl
 
